92b3af3e0b9912b652bdb89ffc2866a07c85ab3e
Victor Martins <martinsvictorsantos@gmail.com>
2025-03-01 14:02:47 -0300

    fiz on decks

--- routes/decks.js
+++ routes/decks.js
@@ -31,75 +31,78 @@ const getFinalData = (arr) => arr.reduce((acc, { deck, win, loss }) => {
 router.get('/', async (req, res) => {
   const { id } = req.query;
   try {
-    const [rows] = await db.query(
-      `  SELECT DISTINCT
-          CAST (d.duelist1 AS CHAR) as duelist1,
-          CAST (d.duelist2 AS CHAR) as duelist2,
-          (SELECT username FROM omega.user_discord_data u WHERE d.duelist1 = u.discord_id) AS duelist1_username,
-          (SELECT avatar FROM omega.user_discord_data u WHERE d.duelist1 = u.discord_id) AS duelist1_avatar,
-          (SELECT displayname FROM omega.user_discord_data u WHERE d.duelist1 = u.discord_id) AS duelist1_displayname,
-          (SELECT username FROM omega.user_discord_data u WHERE d.duelist2 = u.discord_id) AS duelist2_username,
-          (SELECT avatar FROM omega.user_discord_data u WHERE d.duelist2 = u.discord_id) AS duelist2_avatar,
-          (SELECT displayname FROM omega.user_discord_data u WHERE d.duelist2 = u.discord_id) AS duelist2_displayname,
-          d.deck1,
-          d.deck2,
-          d.start,
-          d.end
-        FROM omega.duel d
-        WHERE (d.duelist1 IN (${id}) OR d.duelist2 IN (${id})) AND d.start >= '${LASTBANLIST}' AND d.region = 1
-        ORDER BY d.start DESC;`,
-      [id, id]
-    );
-
-    const result = rows.map(async row => {
-      const sideA = await getDeck(row.deck1)
-      const sideB = await getDeck(row.deck2)
-      const duelist = {
-        id: row.duelist1 === id ? row.duelist1 : row.duelist2,
-        deck: row.duelist1 === id ? sideA : sideB,
-        discord: {
-          username: row.duelist1 === id ? row.duelist1_username : row.duelist2_username,
-          avatar: row.duelist1 === id ? row.duelist1_avatar : row.duelist2_avatar,
-          displayname: row.duelist1 === id ? row.duelist1_displayname : row.duelist2_displayname
-        }
-      };
-
-      const opponent = {
-        id: row.duelist1 === id ? row.duelist2 : row.duelist1,
-        deck: row.duelist1 === id ? sideB : sideA,
-        discord: {
-          username: row.duelist1 === id ? row.duelist2_username : row.duelist1_username,
-          avatar: row.duelist1 === id ? row.duelist2_avatar : row.duelist1_avatar,
-          displayname: row.duelist1 === id ? row.duelist2_displayname : row.duelist1_displayname
-        }
-      };
-
-      // const result = mostFrequentValue([row.result1, row.result2, row.result3].filter(n => n >= 0))
-      const winner = row.duelist1
-      const isWinner = row.duelist1 === id
-      const isDraw = false
-
-      return { duelist, opponent, winner, isWinner, isDraw, start: row.start, end: row.end };
-    });
-
-    const matchHistory = await Promise.all(result)
-    const eventos = matchHistory.map(({ start, end }) => ({ inicio: start, fim: end }))
-
-    // Inicializa a duração total
-    let duracaoTotal = moment.duration(0);
-
-    eventos.forEach(evento => {
-      const inicio = moment(evento.inicio);
-      const fim = moment(evento.fim);
-      duracaoTotal += fim.diff(inicio, "ms");
-    });
-
-    const onlyOpponentDecks = matchHistory.map(({ opponent, isWinner }) => ({ deck: opponent.deck[0]?.archetype, win: isWinner, loss: !isWinner }))
-    const onlyDuelistDecks = matchHistory.map(({ duelist, isWinner }) => ({ deck: duelist.deck[0]?.archetype, win: isWinner, loss: !isWinner }))
-    const opponentDecks = getFinalData(onlyOpponentDecks)
-    const mostUsedArchetypes = getFinalData(onlyDuelistDecks)
-
-    res.json({ success: true, data: { opponentDecks, totalGaming: moment.utc(duracaoTotal).format("HH[h] mm[m]"), mostUsedArchetypes, matchHistory,  }});
+    if (isNaN(id)) res.status(200).json({ success: false, mensagem: 'ID incorreto ou mal formatado.' });
+    else {
+      const [rows] = await db.query(
+        `  SELECT DISTINCT
+            CAST (d.duelist1 AS CHAR) as duelist1,
+            CAST (d.duelist2 AS CHAR) as duelist2,
+            (SELECT username FROM omega.user_discord_data u WHERE d.duelist1 = u.discord_id) AS duelist1_username,
+            (SELECT avatar FROM omega.user_discord_data u WHERE d.duelist1 = u.discord_id) AS duelist1_avatar,
+            (SELECT displayname FROM omega.user_discord_data u WHERE d.duelist1 = u.discord_id) AS duelist1_displayname,
+            (SELECT username FROM omega.user_discord_data u WHERE d.duelist2 = u.discord_id) AS duelist2_username,
+            (SELECT avatar FROM omega.user_discord_data u WHERE d.duelist2 = u.discord_id) AS duelist2_avatar,
+            (SELECT displayname FROM omega.user_discord_data u WHERE d.duelist2 = u.discord_id) AS duelist2_displayname,
+            d.deck1,
+            d.deck2,
+            d.start,
+            d.end
+          FROM omega.duel d
+          WHERE (d.duelist1 IN (${id}) OR d.duelist2 IN (${id})) AND d.start >= '${LASTBANLIST}' AND d.region = 1
+          ORDER BY d.start DESC;`,
+        [id, id]
+      );
+  
+      const result = rows.map(async row => {
+        const sideA = await getDeck(row.deck1)
+        const sideB = await getDeck(row.deck2)
+        const duelist = {
+          id: row.duelist1 === id ? row.duelist1 : row.duelist2,
+          deck: row.duelist1 === id ? sideA : sideB,
+          discord: {
+            username: row.duelist1 === id ? row.duelist1_username : row.duelist2_username,
+            avatar: row.duelist1 === id ? row.duelist1_avatar : row.duelist2_avatar,
+            displayname: row.duelist1 === id ? row.duelist1_displayname : row.duelist2_displayname
+          }
+        };
+  
+        const opponent = {
+          id: row.duelist1 === id ? row.duelist2 : row.duelist1,
+          deck: row.duelist1 === id ? sideB : sideA,
+          discord: {
+            username: row.duelist1 === id ? row.duelist2_username : row.duelist1_username,
+            avatar: row.duelist1 === id ? row.duelist2_avatar : row.duelist1_avatar,
+            displayname: row.duelist1 === id ? row.duelist2_displayname : row.duelist1_displayname
+          }
+        };
+  
+        // const result = mostFrequentValue([row.result1, row.result2, row.result3].filter(n => n >= 0))
+        const winner = row.duelist1
+        const isWinner = row.duelist1 === id
+        const isDraw = false
+  
+        return { duelist, opponent, winner, isWinner, isDraw, start: row.start, end: row.end };
+      });
+  
+      const matchHistory = await Promise.all(result)
+      const eventos = matchHistory.map(({ start, end }) => ({ inicio: start, fim: end }))
+  
+      // Inicializa a duração total
+      let duracaoTotal = moment.duration(0);
+  
+      eventos.forEach(evento => {
+        const inicio = moment(evento.inicio);
+        const fim = moment(evento.fim);
+        duracaoTotal += fim.diff(inicio, "ms");
+      });
+  
+      const onlyOpponentDecks = matchHistory.map(({ opponent, isWinner }) => ({ deck: opponent.deck[0]?.archetype, win: isWinner, loss: !isWinner }))
+      const onlyDuelistDecks = matchHistory.map(({ duelist, isWinner }) => ({ deck: duelist.deck[0]?.archetype, win: isWinner, loss: !isWinner }))
+      const opponentDecks = getFinalData(onlyOpponentDecks)
+      const mostUsedArchetypes = getFinalData(onlyDuelistDecks)
+  
+      res.json({ success: true, data: { opponentDecks, totalGaming: moment.utc(duracaoTotal).format("HH[h] mm[m]"), mostUsedArchetypes, matchHistory,  }});
+    }
   } catch (error) {
     console.error('Erro ao buscar dados:', error);
     res.status(500).json({ success: false, mensagem: 'Erro no servidor.' });
